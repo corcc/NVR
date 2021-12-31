968624d6d45a985294532c6a4c8c131af827429a
TaYaKi71751 <shantian.tw@gmail.com>
2022-01-01 06:01:15 +0900

    Use useCookiesFromBrowser, saveCookiesFromResponse

--- src/Standby.ts
+++ src/Standby.ts
@@ -1,50 +1,29 @@
 import {
 	readHar,
-	getCookies,
-	setCookies,
 	getEntries,
 	filterByReqUrl,
 	parseRequest,
-	request
+	request,
+	useCookiesFromBrowser
 } from './util';
+import { saveCookiesFromResponse } from './util/Response';
 import {
-	Cookie,
 	Json,
 	ReservationEnv
 } from './util/type';
-export async function standby({
+export async function standby ({
 	orgCd,
 	sid
-}: ReservationEnv) {
+}: ReservationEnv): Promise<any> {
 	const har: Json = readHar({});
 	const harEntries = getEntries(har);
 	const standbyEntry = filterByReqUrl(harEntries, '/reservation')[0];
 	let standbyRequest = standbyEntry.request;
 	standbyRequest = parseRequest(standbyRequest);
-	let _cookie = standbyRequest.headers.Cookie.split('; ').map(async (cookieStr: string): Promise<string> => {
-		// Use Cookies From Browser
-		const _eq = cookieStr.indexOf('=');
-		const cookieName = function (c: string) {
-			return c.substring(0, _eq);
-		};
-		const cookieValue = async function (c: string) {
-			let _a: Cookie[] | Cookie = await getCookies({});
-			_a = _a.filter((cookie: any) => {
-				const { name } = cookie;
-				return name == cookieName(c);
-			})[0];
-			const { value } = _a;
-			return value;
-		};
-		const name = cookieName(cookieStr);
-		const value = await cookieValue(cookieStr);
-		return [name, value].join('=');
-	});
-	_cookie = await Promise.all(_cookie);
-	console.log(_cookie);
-	standbyRequest.headers.Cookie = _cookie;
+	standbyRequest = await useCookiesFromBrowser(standbyRequest);
 	// TODO : Use Cookies from Browser => Clean Code
 	const res = await request(standbyRequest);
+	await saveCookiesFromResponse(res);
 	return res;
 }
 
